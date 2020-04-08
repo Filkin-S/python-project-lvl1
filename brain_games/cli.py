ab56747541c0c85f1c02301592978bbf8bb2fa8c
Filkin-S <fsp1988@yandex.ru>
2020-04-08 18:04:37 +0300

    intalled linter and travis + fixed a lot of linter problem

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,9 @@
+"""There will be a file desription."""
+
 import prompt
 
 
 def welcome_user():
+    """Greet user."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+"""There will be a file desription."""
+
 from brain_games.cli import welcome_user
 
 
 def greet():
+    """Print welcome message."""
     print('Welcome to the Brain Games!')
 
 
 def main():
+    """Execute main script."""
     greet()
     welcome_user()
 
